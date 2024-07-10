4117c657387a2ab66629f6a834b43f4d543e4e0b
kels7 <hiketabo@gmail.com>
2024-07-10 14:10:59 +0000

    fix: format with prettier

--- README.md
+++ README.md
@@ -1,21 +1,31 @@
 # token-claim
+
 a website for verifying your participation in token sale
 
-# usage 
+# usage
+
 clone the repo and move into the clone folder then install dependencies
+
 ```
 npm i
 ```
+
 create `.env` file at the root of the clone folder and add database path like this
+
 ```
 DB_PATH=path/to/the.db
 ```
+
 use migrate command to create the database
+
 ```
 npm run db:migrate
 ```
+
 run the app
+
 ```
 npm run dev
 ```
+
 open the indicated url in your browser
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,12 +2,12 @@ import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./src/lib/server/db/schema.ts",
-  out: "./src/lib/server/db/migrations",
-  dialect: "sqlite", // 'postgresql' | 'mysql' | 'sqlite'
-  dbCredentials: {
-    url: process.env.DB_PATH as string,
-  },
-  verbose: true,
-  strict: true,
+    schema: "./src/lib/server/db/schema.ts",
+    out: "./src/lib/server/db/migrations",
+    dialect: "sqlite", // 'postgresql' | 'mysql' | 'sqlite'
+    dbCredentials: {
+        url: process.env.DB_PATH as string,
+    },
+    verbose: true,
+    strict: true,
 });
